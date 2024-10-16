99363214693d73a8486eef8f04932bd59d006a09
Rameses <149032793+r43e5e5@users.noreply.github.com>
2024-10-17 07:53:07 +0800

    chaanges

--- app/src/main/java/com/example/appsample/MainActivity.kt
+++ app/src/main/java/com/example/appsample/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.appsample
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +14,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val imageBtnPatient = findViewById<ImageButton>(R.id.imageBtnPatient)
+        imageBtnPatient.setOnClickListener {
+            val intent = Intent(this, PatientActivity::class.java)
+            startActivity(intent)
+        }
 
         val fab: View = findViewById(R.id.fab)
         fab.setOnClickListener { view ->
